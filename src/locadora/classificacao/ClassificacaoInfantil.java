2f5f37bd6fe0eda4d0b02a07291e8d74d9d99448
paulorisucci1 <111197307+paulorisucci1@users.noreply.github.com>
2023-03-15 13:19:23 -0300

    Update ClassificacaoInfantil.java

--- src/locadora/classificacao/ClassificacaoInfantil.java
+++ src/locadora/classificacao/ClassificacaoInfantil.java
@@ -3,6 +3,7 @@ package locadora.classificacao;
 public class ClassificacaoInfantil extends Classificacao {
 
     public ClassificacaoInfantil() {
+        this.pontuacaoAlugadorFrequente = 1;
         this.valorPorDiaExtra = 1.5;
         this.valorFixo = 1.5;
         this.diasAluguelGratis = 3;
